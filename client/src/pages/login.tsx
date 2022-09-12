b3f8aacb4760b241f705368c95ee46ed3f634654
awaisshah228 <m.awaisshah228@gmail.com>
2022-09-12 13:05:13 +0500

    login page added

---
+++ client/src/pages/login.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import PageLogin from '../containers/PageLogin'
+
+const Login = () => {
+  return (
+    <div>
+        <PageLogin />
+    </div>
+  )
+}
+
+export default Login
