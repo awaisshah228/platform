38ad895774571a7e1adf5b00cf7aeb2de9dd1aa8
awaisshah228 <m.awaisshah228@gmail.com>
2022-09-13 13:57:57 +0500

    donen

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -88,9 +88,9 @@ module.exports = {
     },
   },
   plugins: [
-    // require("@tailwindcss/typography"),
-    // require("@tailwindcss/forms"),
-    // require("@tailwindcss/line-clamp"),
-    // require("@tailwindcss/aspect-ratio"),
+    require("@tailwindcss/typography"),
+    require("@tailwindcss/forms"),
+    require("@tailwindcss/line-clamp"),
+    require("@tailwindcss/aspect-ratio"),
   ],
 };
