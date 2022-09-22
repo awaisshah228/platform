2b86702aab7e6f7db648b2f7abf0808066894b45
awaisshah228 <m.awaisshah228@gmail.com>
2022-09-22 17:32:04 +0500

    done

--- server/src/routes/userRouter.ts
+++ server/src/routes/userRouter.ts
@@ -21,6 +21,6 @@ router.patch("/reset_password", requireAuth,[ body("password")
 .isLength({ min: 4, max: 20 })
 .withMessage("Password must be between 4 and 20 characters"),],validateRequest, userCtrl.resetPassword);
 
-router.get("/:id",requireAuth, userCtrl.getUser);
+router.get("/:id", userCtrl.getUser);
 
 export default router;
