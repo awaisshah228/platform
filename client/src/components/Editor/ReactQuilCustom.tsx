b7c87a4f0dfd2520f5b9f5128e865d593904c5ab
awaisshah228 <m.awaisshah228@gmail.com>
2022-09-28 00:51:49 +0500

    image upload in blog added

--- client/src/components/Editor/ReactQuilCustom.tsx
+++ client/src/components/Editor/ReactQuilCustom.tsx
@@ -1,7 +1,13 @@
-import React, { useRef } from 'react'
+import React, { useCallback, useEffect, useRef } from 'react'
 import ReactQuill from 'react-quill';
+import { toast } from "react-toastify";
+import { useAppDispatch } from '../../app/hook';
+import { postAPI } from './../../utils/fetchData';
+
 
 const ReactQuilCustom = ({field}) => {
+
+    const dispatch=useAppDispatch()
     const quillRef = useRef<ReactQuill>(null);
     const container = [
       [{ font: [] }],
@@ -23,6 +29,89 @@ const ReactQuilCustom = ({field}) => {
   
   
     const modules = { toolbar: { container } };
+
+     const checkImage = (file: File) => {
+      const types = ['image/png', 'image/jpeg']
+      let err = ''
+      if(!file) return err = "File does not exist."
+    
+      if(file.size > 1024 * 1024) // 1mb
+        err = "The largest image size is 1mb"
+    
+      if(!types.includes(file.type))
+        err = "The image type is png / jpeg"
+    
+      return err;
+    }
+    
+     const imageUpload = async (file: File) => {
+      const formData = new FormData()
+      formData.append("image", file)
+     
+    
+      const res:any = await postAPI('/image',formData)
+    
+      const data = await res.json()
+      return {  url: data.secure_url };
+    }
+    // Custom image
+  const handleChangeImage = useCallback(() => {
+    const input = document.createElement('input')
+    input.type = "file"
+    input.accept = "image/*"
+    input.click()
+
+    input.onchange = async () => {
+      const files = input.files
+      if(!files) {
+        toast("File does not exist", {
+          position: "top-right",
+          autoClose: 5000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          });
+          return;
+
+      }
+
+      const file = files[0]
+      const check = checkImage(file)
+      if(check) {
+        toast(check, {
+          position: "top-right",
+          autoClose: 5000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          });
+          return;
+      }
+      
+      const photo = await imageUpload(file)
+
+      const quill = quillRef.current;
+      const range = quill?.getEditor().getSelection()?.index
+      if(range !== undefined){
+        quill?.getEditor().insertEmbed(range, 'image', `${photo.url}`)
+      }
+
+    
+    }
+  },[dispatch])
+
+
+  useEffect(() => {
+    const quill = quillRef.current;
+    if(!quill) return;
+
+    let toolbar = quill.getEditor().getModule('toolbar')
+    toolbar.addHandler('image', handleChangeImage)
+  },[handleChangeImage])
 
   return (
     <ReactQuill
